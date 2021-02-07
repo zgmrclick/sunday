566a6daa46ab0f43c51f05fe4041e85555fedf12
zgmrclick <zg.mrclick@gmail.com>
2021-02-07 17:28:35 +0200

    hi!

---
+++ Loop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 4;
+            int i = 0;
+
+            //while (i < x)
+            //{
+            //    int a = 0;
+            //    while (a < x)
+            //    {
+            //        Console.Write("*");
+            //        a++;
+            //    }
+            //    i++;
+            //    Console.WriteLine();
+            //}
+
+            for (int i = 0; i < 3; i++);
+            {
+                for (int j = 0; j < i; j++);
+                {
+                    Console.WriteLine("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
